0e25ee7bfb0f9ede1ee595301eafc2062a4614a9
digital-kid <86312605+digital-kid@users.noreply.github.com>
2021-07-01 03:59:46 -0700

    Add files via upload

---
+++ variables,data types and type casting.py
@@ -0,0 +1,21 @@
+# how to store a variable
+var1=1
+var2=1.5
+var3="Jamil"
+var4="2"
+var4=int(var4)
+#type casting
+'''
+var1 is an integer
+var 2 is a float
+var3 is a string
+var4 is also a string
+but it's possible to change it to an integer simply you will write"var4=int(var4)"
+'''
+
+#data types in python
+'''
+var1 is an integer
+var 2 is a float
+var3 is a string
+'''
