973e3817afd76debe4036c0e173090cc546a024c
digital-kid <86312605+digital-kid@users.noreply.github.com>
2021-07-01 03:46:57 -0700

    Add files via upload

---
+++ comments,escape sequences&print statement.py
@@ -0,0 +1,16 @@
+#There are two type of comments 
+#comments(this is a comment)(single line comment)
+'''This is a multi line comment
+as
+you can see'''#multi-line comment
+#print statements
+print("Jamil is a good boy")#used for single line print statement
+print('Jamil is a good boy')#used for single line print statement
+print('''Jamil is a good boy,
+he is eleven years old
+''')#used for multi-line print statement
+#you can also do this
+print("A quick brown fox jumped over")
+print("The lazy dog")#these two statements would be in different line if you dont want this try this
+print("A quick brown fox jumped over", end=" ")
+print("The lazy dog")
