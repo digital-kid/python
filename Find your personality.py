4495a7715e915ca6a1e872e9d9c4f7a3828ee84d
digital-kid <86312605+digital-kid@users.noreply.github.com>
2021-07-02 07:22:00 -0700

    Add files via upload

---
+++ Find your personality.py
@@ -0,0 +1,428 @@
+print("This is just a fun program")
+dict1={}
+dict1["a"]= "cool"
+dict1["b"]= "Normal"
+dict1["c"]= "Bad"
+dict1["d"]= "Intelligent"
+dict1["e"]= "Good"
+dict1["f"]= "Funny"
+dict1["g"]= "Funny"
+dict1["h"]= "Genius"
+dict1["i"]= "Amazing"
+dict1["j"]= "Perfect"
+dict1["k"]= "Evil"
+dict1["l"]= "Funny"
+dict1["m"]= "Always using smartphone"
+dict1["n"]= "Cool"
+dict1["o"]= "Sensitive"
+dict1["p"]= "Intelligent"
+dict1["q"]= "Amazing"
+dict1["r"]= "Brave"
+dict1["s"]= "Cool"
+dict1["t"]= "Adventure lover"
+dict1["u"]= "Genius"
+dict1["v"]= "Brave"
+dict1["w"]= "Normal"
+dict1["x"]= "Amazing"
+dict1["y"]= "Funny"
+dict1["z"]= "Evil"
+dict1["A"]= "cool"
+dict1["B"]= "Normal"
+dict1["C"]= "Bad"
+dict1["D"]= "Intelligent"
+dict1["E"]= "Good"
+dict1["F"]= "Funny"
+dict1["G"]= "Funny"
+dict1["H"]= "Genius"
+dict1["I"]= "Amazing"
+dict1["J"]= "Perfect"
+dict1["K"]= "Evil"
+dict1["L"]= "Funny"
+dict1["M"]= "Always using smartphone"
+dict1["N"]= "Cool"
+dict1["O"]= "Sensitive"
+dict1["P"]= "Intelligent"
+dict1["Q"]= "Amazing"
+dict1["R"]= "Brave"
+dict1["S"]= "Cool"
+dict1["T"]= "Adventure lover"
+dict1["U"]= "Genius"
+dict1["V"]= "Brave"
+dict1["W"]= "Normal"
+dict1["X"]= "Amazing"
+dict1["Y"]= "Funny"
+dict1["Z"]= "Evil"
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
+
+
+a=input("Enter First letter of your Name: ")
+print("You are ",(dict1.get(a)))
